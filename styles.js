1b32f57d8808e2133bd20e5776d229465c24e969
ZenChris <121231422+ZenChris18@users.noreply.github.com>
2024-07-12 16:16:36 +0800

    Finished project implemented a scoring system and updated my if else in playround function

--- styles.js
+++ styles.js
@@ -3,11 +3,12 @@ let computerScore = 0;
 
 function getComputerChoice(){
     let number = Math.floor(Math.random() * 3);
-    return number
+    return number;
 }
+
 function getHumanChoice(){
-    let human
-    let pick = prompt("Enter you choice (rock, paper, scissors)").toLowerCase();
+    let human;
+    let pick = prompt("Enter your choice (rock, paper, scissors)").toLowerCase();
     if(pick === "rock"){
         human = 0;
     }
@@ -18,53 +19,66 @@ function getHumanChoice(){
         human = 2;
     }
     else{
-        console.log("Pick from the Choices only")
+        console.log("Pick from the Choices only");
+        return getHumanChoice(); // Ask again if invalid choice
     }
-    return human
+    return human;
 }
-
 
 function playRound(humanChoice, computerChoice){
     if(humanChoice === computerChoice){
-        console.log("TIE!")
+        console.log("TIE!");
+        return 'tie';
     }
-    else if(humanChoice != computerChoice){
-        if(humanChoice=== 0){
-            if(computerChoice === 1){
-                console.log("You lose! You picked rock while the computer picked paper");
-            }
-            else if(computerChoice === 2){
-                console.log("You win! You picked rock while the computer picked scissors");
-            }
-        }
-        else if(humanChoice ===1){
-            if(computerChoice === 0){
-                console.log("You Win! You picked paper while the computer picked rock");
-            }
-            else if(computerChoice === 2){
-                console.log("You Lose! You picked paper while the computer picked scissors");
-            }
-        }
-        else if(humanChoice === 2){
-            if(computerChoice === 0){
-                console.log("You Lose! You picked scissors while the computer picked rock");
-            }
-            else if(computerChoice === 1){
-                console.log("You win! You picked scissors while the computer picked paper");
-            }
-        }
+    else if(humanChoice === 0 && computerChoice === 1){
+        console.log("You lose! You picked rock while the computer picked paper");
+        return 'computer';
+    }
+    else if(humanChoice === 0 && computerChoice === 2){
+        console.log("You win! You picked rock while the computer picked scissors");
+        return 'human';
+    }
+    else if(humanChoice === 1 && computerChoice === 0){
+        console.log("You win! You picked paper while the computer picked rock");
+        return 'human';
+    }
+    else if(humanChoice === 1 && computerChoice === 2){
+        console.log("You lose! You picked paper while the computer picked scissors");
+        return 'computer';
+    }
+    else if(humanChoice === 2 && computerChoice === 0){
+        console.log("You lose! You picked scissors while the computer picked rock");
+        return 'computer';
+    }
+    else if(humanChoice === 2 && computerChoice === 1){
+        console.log("You win! You picked scissors while the computer picked paper");
+        return 'human';
     }
 }
 
-
-
 function playGame(){
-    let i =0;
     let times = 5;
     for(let i = 0; i < times; i++){
         const humanSelection = getHumanChoice();
         const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
+        const result = playRound(humanSelection, computerSelection);
+
+        if (result === 'human') {
+            humanScore++;
+        } else if (result === 'computer') {
+            computerScore++;
+        }
+
+        console.log(`Score: Human ${humanScore} - Computer ${computerScore}`);
+    }
+
+    console.log(`Final Score: Human ${humanScore} - Computer ${computerScore}`);
+    if (humanScore > computerScore) {
+        console.log("you won the game!");
+    } else if (computerScore > humanScore) {
+        console.log("Computer wins the game!");
+    } else {
+        console.log("The game is a draw!");
     }
 }
 
